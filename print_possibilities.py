e16715ee4c95737fd313c42f293f03d5ee360220
c7i <46483067+c7i@users.noreply.github.com>
2019-12-27 19:19:14 +0100

    add example usage

---
+++ print_possibilities.py
@@ -0,0 +1,12 @@
+from sequence_list import SequenceList
+
+six_digits = input(':: Enter first six digits or birth date (format: ddmmyy): ')
+gender = input(':: Enter gender as m/f: ')
+year = input(':: Enter year: ')
+print('Printing possibilities...')
+
+seq_list = SequenceList(six_digits, gender, year)
+for i in seq_list: print(six_digits + '-' + i)
+
+print('Number of possibilities: ' + str(len(seq_list)))
+
